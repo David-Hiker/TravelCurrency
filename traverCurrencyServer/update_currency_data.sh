9905f1148a3d4747f874427a3c0afebcd2573169
David-Hiker <hiker322@gmail.com>
2017-06-26 13:12:43 +0800

    Update update_currency_data.sh

--- traverCurrencyServer/update_currency_data.sh
+++ traverCurrencyServer/update_currency_data.sh
@@ -4,7 +4,7 @@
 
 pathdir="/data/release/TravelCurrencyApp"
 
-curl -s --get --include 'http://ali-waihui.showapi.com/bank10?bankCode=ICBC'  -H 'Authorization:APPCODE 97b2db3639f9401cb7f822545cc637e3'|grep showapi_res_body|jq '.showapi_res_body.codeList' > $pathdir/currency_rawdata.json
+curl -s --get --include 'http://ali-waihui.showapi.com/bank10?bankCode=ICBC'  -H 'Authorization:APPCODE '|grep showapi_res_body|jq '.showapi_res_body.codeList' > $pathdir/currency_rawdata.json
 
 #curl https://www.nethunter.cn:4430/tcurrency
 
